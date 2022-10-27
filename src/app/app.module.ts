ac3c401893aaf35c9e6874bd5bdc1948a1e7d016
leviosa19 <jaderushi2014@gmail.com>
2022-10-27 21:25:54 +0530

    added timeline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './common/timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
